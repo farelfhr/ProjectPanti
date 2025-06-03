877d9bc2c23b2e14d7e444ba902945f4c9b3f9db
MReyAkbar <reyhanakbar90@gmail.com>
2025-06-03 15:10:43 +0700

    modif model user

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,6 +6,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use illuminate\Database\Eloquent\Relations\HasMany;
 
 class User extends Authenticatable
 {
@@ -44,5 +45,18 @@ class User extends Authenticatable
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
         ];
+    }
+
+    public function articles(): HasMany
+    {
+        return $this->hasMany(Artikel::class, 'id_penulis');
+    }
+
+    /**
+     * Get the contact messages sent by the user.
+     */
+    public function contactMessages(): HasMany
+    {
+        return $this->hasMany(Kontak::class, 'id_user');
     }
 }
