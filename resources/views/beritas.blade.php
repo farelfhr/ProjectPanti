1be27b18ae075b54309f7a65e888c576c33df2ac
Ferdi Dwana <ferdiansyahdwana@gmail.com>
2025-06-17 09:55:19 +0700

    Update UI Page berita single post

--- resources/views/beritas.blade.php
+++ resources/views/beritas.blade.php
@@ -1,13 +1,27 @@
 @extends('layouts.app')
 @section('content')
+<div class="py-20 md:py-28 bg-white relative -mt-32">
+  <div class="absolute inset-0 opacity-3">
+      <div class="absolute top-20 right-10 w-32 h-32 bg-[#D0D5CB] rounded-full animate-float"></div>
+      <div class="absolute bottom-20 left-10 w-24 h-24 bg-[#F1F0E9] rounded-full animate-float" style="animation-delay: 1s;"></div>
+  </div>
 
-<div class="max-w-4xl mx-auto py-16 px-4">
-  <div class="p-6 bg-[#f1f0e9] rounded-lg shadow-md hover:shadow-xl transition-all duration-300" href="/berita/{{ $beritas['id_artikel'] }}">
-    <h3 class="text-4xl text-center py-8 font-bold mb-4">{{ $beritas['judul'] }}</h3>
-    <img class="w-96 h-auto mx-auto mb-12 rounded-lg shadow-md object-cover" src="{{ $beritas['gambar'] }}" alt="Artikel">
-    <p class="text-lg mb-4">{{ $beritas['konten'] }}</p>
-    <p class="text-sm mb-4">{{ $beritas->publish_date->diffForHumans() }} <span class="text-[#41644A] font-bold">{{ $beritas['penulis'] }}</span></p>
-    <a class="text-[#E9762B] hover:underline font-bold" href="/berita">kembali</a>
+  <div class="max-w-3xl mx-auto px-6 relative z-10">
+    <div class="" href="/berita/{{ $beritas['id_artikel'] }}">
+      <div class="flex flex-col gap-6">
+        <h3 class="text-5xl pt-8 font-bold">{{ $beritas['judul'] }}</h3>
+        <p class="text-lg uppercase text-[#41644A] font-bold">{{ $beritas->kategori->nama }}</p>
+        <img class="w-96 h-auto mx-auto rounded-lg shadow-md object-cover" src="{{ $beritas['gambar'] }}" alt="Artikel">
+        <p class="text-lg">{{ $beritas['konten'] }}</p>
+      
+        <div class="flex gap-2">
+          <p class="font-bold">{{ $beritas->author->name }}</p>
+          <span>|</span>
+          <p class="font-bold">{{ $beritas->publish_date->diffForHumans() }}</p>
+        </div>
+
+        <a class="text-[#E9762B] font-bold hover:underline" href="/berita">kembali</a>
+      </div>
   </div>
 </div>
 
