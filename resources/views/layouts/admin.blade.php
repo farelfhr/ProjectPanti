8c28ed013631492d7b7a58fc8e123f0b3ea376f6
farelfhr <farelfathir008@gmail.com>
2025-06-24 18:02:48 +0700

    admin route

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -18,6 +18,8 @@
                 <a href="{{ route('admin.artikel.index') }}" class="block py-2.5 px-4 rounded transition duration-200 hover:bg-gray-700">Manajemen Artikel</a>
                 <a href="{{ route('admin.panti.index') }}" class="block py-2.5 px-4 rounded transition duration-200 hover:bg-gray-700">Manajemen Panti</a>
                 <a href="{{ route('admin.kategori.index') }}" class="block py-2.5 px-4 rounded transition duration-200 hover:bg-gray-700">Manajemen Kategori</a>
+                <a href="{{ route('admin.kegiatan.index') }}" class="block py-2.5 px-4 rounded transition duration-200 hover:bg-gray-700">Manajemen Kegiatan</a>
+                <a href="{{ route('admin.faqs.index') }}" class="block py-2.5 px-4 rounded transition duration-200 hover:bg-gray-700">Manajemen FAQ</a>
                 <a href="{{ route('admin.kontak.index') }}" class="block py-2.5 px-4 rounded transition duration-200 hover:bg-gray-700">Pesan Masuk</a>
                 <a href="{{ route('admin.users.index') }}" class="block py-2.5 px-4 rounded transition duration-200 hover:bg-gray-700">Manajemen Pengguna</a>
                 {{-- Tambahkan link manajemen lain di sini (Panti, Kategori, dll) --}}
